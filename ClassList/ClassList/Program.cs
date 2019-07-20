2b08be6c6f36b61bb2b0459e30c25406c4278ceb
Bohdan <bohdan.hukivskyi@outlook.com>
2019-07-20 16:58:13 +0300

    VladList is over

---
+++ ClassList/ClassList/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VladList<int> vladList = new VladList<int>();
+            for (int i = 0; i <= 1000; i++)
+            {
+                vladList.Add(i);
+            }
+            int j = 1000;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine(vladList[j]);
+                    j--;
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    break;
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ClassList/ClassList/VladList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassList
+{
+    class VladList<TElement>
+    {
+        public VladList()
+        {
+            List = new TElement[101];
+        }
+        private TElement[] List;
+        private int index;
+        public void Add(TElement num)
+        {
+            if (List.Length <= index)
+            {
+                Array.Copy(List, 0, List = new TElement[List.Length * 2], 0, List.Length / 2);
+            }
+            List[index] = num;
+            index++;
+        }
+        public TElement this[int index]
+        {
+            get
+            {
+                if (index > this.index)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return List[index];
+            }
+            set
+            {
+                if (index > this.index)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                List[index] = value;
+            }
+        }
+    }
+}
